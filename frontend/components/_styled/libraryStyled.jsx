f44f077d43c3348b375e1fdd378d4a9643468602
Seulgigi <2021112414@dgu.ac.kr>
2024-10-15 19:17:26 +0900

    [FE]Feat: Add Book 페이지 연결 버튼 추가

--- frontend/components/_styled/libraryStyled.jsx
+++ frontend/components/_styled/libraryStyled.jsx
@@ -1,1 +1,24 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const LibraryAdd = styled.div`
+  width: 90%;
+  margin: 5%;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+`;
+
+export const LibraryButtonBox = styled.div`
+  width: 40px;
+  height: 40px;
+  background-color: #cbd5e1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
+
+export const LibraryAddButton = styled(FontAwesomeIcon)`
+  font-size: 24px;
+`;
--- frontend/pages/library/index.jsx
+++ frontend/pages/library/index.jsx
@@ -1,10 +1,24 @@
 import Header from "@/components/layout/Header";
 import * as MS from "../../components/_styled/mainStyled";
+import * as LS from "../../components/_styled/libraryStyled";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+import { useRouter } from "next/router";
 
 export default function Library() {
+  const router = useRouter();
   return (
     <MS.MainContainer>
       <Header path="My Library" />
+      <LS.LibraryAdd>
+        <LS.LibraryButtonBox
+          onClick={() => {
+            router.push(`/library/add`);
+          }}
+        >
+          <LS.LibraryAddButton icon={faPlus} />
+        </LS.LibraryButtonBox>
+      </LS.LibraryAdd>
     </MS.MainContainer>
   );
 }
