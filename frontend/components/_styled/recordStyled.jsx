3a9c8453d28f4d3e53330239a5dedf67cda812ed
Seulgigi <2021112414@dgu.ac.kr>
2024-10-19 15:36:45 +0900

    [FE]Feat: record 페이지 타입 버튼

--- frontend/components/_styled/recordStyled.jsx
+++ frontend/components/_styled/recordStyled.jsx
@@ -1,1 +1,23 @@
 import styled from "styled-components";
+
+export const RecordTap = styled.div`
+  width: 90%;
+  margin: 0 5%;
+  display: flex;
+  align-items: center;
+`;
+
+export const RecordTapButton = styled.div`
+  width: 50%;
+  padding: 4%;
+  border-radius: 10px;
+  font-size: 14px;
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.4s ease;
+
+  color: ${(props) => (props.$isActive ? `#f8fafc` : `#64748B`)};
+  background-color: ${(props) => (props.$isActive ? `#64748b` : `#E2E8F0`)};
+`;
--- frontend/pages/record/index.jsx
+++ frontend/pages/record/index.jsx
@@ -1,11 +1,37 @@
 import Header from "@/components/layout/Header";
 import * as MS from "../../components/_styled/mainStyled";
+import * as RS from "../../components/_styled/recordStyled";
+import { useState } from "react";
 
 export default function Record() {
+  const [currentTap, setCurrentTap] = useState("month");
+  const tapList = [
+    { type: "month", content: "월" },
+    { type: "routine", content: "루틴" },
+  ];
+  const handleCurrentTap = (type) => {
+    setCurrentTap(type);
+  };
+
   return (
     <MS.MainWrapper>
       <Header path="My Record" />
-      <MS.MainContainer>record</MS.MainContainer>
+      <MS.MainContainer>
+        {/* type 선택 */}
+        <RS.RecordTap>
+          {tapList.map((tap, idx) => (
+            <RS.RecordTapButton
+              key={idx}
+              $isActive={tap.type === currentTap}
+              onClick={() => handleCurrentTap(tap.type)}
+            >
+              {tap.content}별 루틴
+            </RS.RecordTapButton>
+          ))}
+        </RS.RecordTap>
+
+        {/* type별 */}
+      </MS.MainContainer>
     </MS.MainWrapper>
   );
 }
