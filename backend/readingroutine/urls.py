57e7f9c9d9fd9a26073c86714b8d3a74bc5eea88
ParkSuhhyun <2022111511@dgu.ac.kr>
2024-11-04 01:09:08 +0900

    [BE]Feat: settings.py 수정

--- backend/readingroutine/urls.py
+++ backend/readingroutine/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/routinelist/',include('routinelist.urls')),
 ]
