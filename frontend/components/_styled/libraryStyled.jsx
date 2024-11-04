2773277e6f3a5bb1ad6452024f0f38e30b2941f6
Seulgigi <2021112414@dgu.ac.kr>
2024-11-04 16:19:46 +0900

    [FE]Feat: Add Book 페이지

--- frontend/components/_styled/libraryStyled.jsx
+++ frontend/components/_styled/libraryStyled.jsx
@@ -57,3 +57,76 @@ export const LibraryList = styled.div`
   justify-content: space-between;
   align-items: flex-start;
 `;
+
+// add book
+export const AddBookContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  height: calc(100vh);
+`;
+
+export const AddBookInputSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 5%;
+`;
+
+export const AddBookInputText = styled.div`
+  width: 100%;
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 5%;
+`;
+
+export const AddBookInput = styled.input`
+  font-size: 14px;
+  width: 100%;
+  padding: 5%;
+  outline: none;
+  border: none;
+  border-radius: 10px;
+  background-color: #f1f4f7;
+  color: #64748b;
+  font-weight: 500;
+
+  ::placeholder {
+    color: #64748b;
+  }
+`;
+
+export const AddBookTextArea = styled.textarea`
+  font-size: 14px;
+  line-height: 20px;
+  width: 100%;
+  height: 20vh;
+  padding: 5%;
+  outline: none;
+  border: none;
+  border-radius: 10px;
+  background-color: #f1f4f7;
+  color: #64748b;
+  font-weight: 500;
+  resize: none;
+
+  ::placeholder {
+    color: #64748b;
+  }
+`;
+
+export const AddBookSubmitButton = styled.div`
+  cursor: pointer;
+  width: 90%;
+  margin: 5%;
+  padding: 5%;
+  background-color: #cbd5e1;
+  color: #0f172a;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 500;
+`;
--- frontend/pages/library/add.jsx
+++ frontend/pages/library/add.jsx
@@ -1,5 +1,37 @@
 import * as LS from "../../components/_styled/libraryStyled";
+import * as HCS from "../../components/layout/Styled";
 
 export default function AddLibrary() {
-  return <>add</>;
+  return (
+    <LS.LibraryWrapper>
+      {/* 책 제목 검색 모달 */}
+      <LS.AddBookContainer>
+        <HCS.HeaderContainer>
+          <HCS.HeaderPath>Add Book</HCS.HeaderPath>
+        </HCS.HeaderContainer>
+
+        <LS.AddBookInputSection>
+          <LS.AddBookInputText>Title</LS.AddBookInputText>
+          <LS.AddBookInput placeholder="책 제목" type="text" />
+        </LS.AddBookInputSection>
+
+        <LS.AddBookInputSection>
+          <LS.AddBookInputText>Writer</LS.AddBookInputText>
+          <LS.AddBookInput placeholder="작가" type="text" />
+        </LS.AddBookInputSection>
+
+        <LS.AddBookInputSection>
+          <LS.AddBookInputText>Publisher</LS.AddBookInputText>
+          <LS.AddBookInput placeholder="출판사" type="text" />
+        </LS.AddBookInputSection>
+
+        <LS.AddBookInputSection>
+          <LS.AddBookInputText>Summary</LS.AddBookInputText>
+          <LS.AddBookTextArea type="text" />
+        </LS.AddBookInputSection>
+
+        <LS.AddBookSubmitButton>완료</LS.AddBookSubmitButton>
+      </LS.AddBookContainer>
+    </LS.LibraryWrapper>
+  );
 }
