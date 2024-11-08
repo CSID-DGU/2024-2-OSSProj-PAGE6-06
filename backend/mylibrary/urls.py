84e564fb3a244e1ad0fa2495b7116d02c762f3f1
gaaaani (서가은) <155754445+gaaaani@users.noreply.github.com>
2024-11-09 01:45:37 +0900

    Revert "[BE]Feat: mystats 생성"

--- backend/mylibrary/urls.py
+++ backend/mylibrary/urls.py
@@ -1,8 +1,12 @@
 # mylibrary/urls.py
-from django.urls import path
-from .views import search_books, save_selected_book
+from django.urls import path, include  # path와 include 임포트
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, search_books
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+# router.register(r'routines', RoutineRecordViewSet) # RoutineRecordViewSet
 
 urlpatterns = [
-    path('books/search/', search_books, name='search_books'),  # 책 검색 API
-    path('books/save/', save_selected_book, name='save_selected_book'),  # 선택된 책 저장 API
-]
+    path('books/search/', search_books, name='search_books'),
+] + router.urls
