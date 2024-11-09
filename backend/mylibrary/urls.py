79d0e4ccb5de5dc3f68a2520aaaa781f1ea0f380
gaaaani <geseo0523@gmail.com>
2024-11-09 13:48:26 +0900

    [BE]Feat: mylibrary 루틴 수정/삭제 기능 추가

--- backend/mylibrary/urls.py
+++ backend/mylibrary/urls.py
@@ -1,8 +1,9 @@
 # mylibrary/urls.py
 from django.urls import path
-from .views import search_books, save_selected_book
+from .views import search_books, save_selected_book, routine_complete_detail
 
 urlpatterns = [
     path('books/search/', search_books, name='search_books'),  # 책 검색 API
     path('books/save/', save_selected_book, name='save_selected_book'),  # 선택된 책 저장 API
+    path('books/routines/<int:routineCompleteId>/', routine_complete_detail, name='routine_complete_detail'),  # 루틴 조회, 수정, 삭제 API
 ]
