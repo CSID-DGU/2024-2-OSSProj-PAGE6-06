986217c951036068dc20ce8f7425cce8a446144a
gaaaani <geseo0523@gmail.com>
2024-11-11 16:10:34 +0900

    [BE]Fix: 충돌 수정

--- backend/mystats/urls.py
+++ backend/mystats/urls.py
@@ -1,7 +1,6 @@
-# mystats/urls.py 
+# mystats/urls.py
 from django.urls import path
 from .views import RoutinesByDateAPIView, RoutineRecordView
-
 urlpatterns = [
     path('record/date/', RoutinesByDateAPIView.as_view(), name='routines-by-date'),  # 쿼리 파라미터를 사용하여 날짜 전달
     path('record/<str:routine>/', RoutineRecordView.as_view(), name='routine-record'),
