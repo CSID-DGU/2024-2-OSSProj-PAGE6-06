bfc2537e4a6c85d162ac651a38be6b26bfa5b744
ParkSuhhyun <2022111511@dgu.ac.kr>
2024-11-21 16:44:36 +0900

    [BE]Fix: settings.py json 응답으로 수정

--- backend/readingroutine/settings.py
+++ backend/readingroutine/settings.py
@@ -60,6 +60,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 
