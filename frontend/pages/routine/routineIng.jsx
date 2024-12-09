56b98736439e46ab39b974488391fbbadfc09b2e
swim-kim <ksoo5386@naver.com>
2024-12-09 20:24:17 +0900

    fix: 루틴진행중 뒤로가기 했을때 값 처리

--- frontend/pages/routine/routineIng.jsx
+++ frontend/pages/routine/routineIng.jsx
@@ -1,8 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import * as RS from "@/components/_styled/routineStyled";
 import Timer from '@/components/routine/routineIng/Timer';
 import TimerInfo from '@/components/routine/routineIng/TimerInfo';
-export default function routineIng() {
+
+export default function RoutineIng() {
+
+    useEffect(() => {
+        const handlePopState = () => {
+            // 뒤로 가기 시 로컬스토리지 값 삭제
+            localStorage.removeItem("routineContent");
+            localStorage.removeItem("routineId");
+            localStorage.removeItem("routineTime");
+            localStorage.removeItem("routineTitle");
+        };
+
+        window.addEventListener("popstate", handlePopState);
+        return () => {
+            window.removeEventListener("popstate", handlePopState);
+        };
+    }, []);
 
     return (
         <RS.RoutineIngPage>
@@ -10,8 +26,6 @@ export default function routineIng() {
             <TimerInfo />
             <Timer />
           </RS.RoutineIngContainer>
-          
         </RS.RoutineIngPage>
     );
 }
-
