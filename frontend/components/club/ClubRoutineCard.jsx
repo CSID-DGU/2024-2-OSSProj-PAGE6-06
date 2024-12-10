33a9bd72507455b77558294bed8b3e20b40006ae
Seulgigi <2021112414@dgu.ac.kr>
2024-12-10 19:31:37 +0900

    [FE]Fix: club card 이미지

--- frontend/components/club/ClubRoutineCard.jsx
+++ frontend/components/club/ClubRoutineCard.jsx
@@ -4,59 +4,32 @@ import profile1 from "../common/image/profile1.png";
 import profile2 from "../common/image/profile2.png";
 import profile3 from "../common/image/profile3.png";
 import profile4 from "../common/image/profile4.png";
-import { useEffect, useState } from "react";
-import { API } from "@/pages/api";
 
 export default function ClubRoutineCard({ routine }) {
   const formattedDate = routine.date.split("T")[0];
 
-  const [profileImage, setProfileImage] = useState("");
-
-  const fetchUserImage = async () => {
-    try {
-      const token = localStorage.getItem("token");
-
-      const response = await API.get(`/profile`, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      });
-      setProfileImage(response.data.profile_image);
-    } catch (e) {
-      console.log(e);
+  const getProfileImage = (profileImage) => {
+    switch (profileImage) {
+      case "image1":
+        return profile1;
+      case "image2":
+        return profile2;
+      case "image3":
+        return profile3;
+      case "image4":
+        return profile4;
+      default:
+        return profile1;
     }
   };
-
-  useEffect(() => {
-    fetchUserImage();
-  }, []);
 
   return (
     <RCS.RecordCardContainer>
       <S.ClubRoutineCardUser>
-        {profileImage === "image1" ? (
-          <S.ClubRoutineCardUserImage src={profile1} alt="profile_image" />
-        ) : (
-          <>
-            {profileImage === "image2" ? (
-              <S.ClubRoutineCardUserImage src={profile2} alt="profile_image" />
-            ) : (
-              <>
-                {profileImage === "image3" ? (
-                  <S.ClubRoutineCardUserImage
-                    src={profile3}
-                    alt="profile_image"
-                  />
-                ) : (
-                  <S.ClubRoutineCardUserImage
-                    src={profile4}
-                    alt="profile_image"
-                  />
-                )}
-              </>
-            )}
-          </>
-        )}
+        <S.ClubRoutineCardUserImage
+          src={getProfileImage(routine.userImage)}
+          alt="profile_image"
+        />
         <S.ClubRoutineCardUserInfo>
           <S.ClubRoutineCardUserInfoName>
             {routine.userNickname}
